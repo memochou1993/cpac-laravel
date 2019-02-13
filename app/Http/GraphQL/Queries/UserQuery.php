a0e2844729c396db8e449011a59d8d4295871119
William <william@fontrip.com>
2019-02-13 17:35:10 +0800

    新增 UserQuery 查詢類別

---
+++ app/Http/GraphQL/Queries/UserQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\GraphQL\Queries;
+
+use App\User;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class UserQuery
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue
+     * @param  array  $args
+     * @param  GraphQLContext|null  $context
+     * @param  ResolveInfo  $resolveInfo
+     * @return mixed
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        return User::paginate($args['pagination'] ?? config('default.pagination'));
+    }
+}
